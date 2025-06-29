ca865275fc352c5fcbeaf41ed23e48666da41071
Enceka <enceka@yeah.net>
2025-06-29 19:45:16 +0800

    Fix left renames

--- twrp_U30Air.mk
+++ twrp_U30Air.mk
@@ -9,14 +9,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Omni stuff.
-$(call inherit-product, vendor/omni/config/common.mk)
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
 
 # Inherit from U30Air device
 $(call inherit-product, device/zte/U30Air/device.mk)
 
 PRODUCT_DEVICE := U30Air
-PRODUCT_NAME := omni_U30Air
+PRODUCT_NAME := twrp_U30Air
 PRODUCT_BRAND := ZTE
 PRODUCT_MODEL := U30 Air
 PRODUCT_MANUFACTURER := zte
