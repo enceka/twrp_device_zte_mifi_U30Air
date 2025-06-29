fc8acf570342ca66e37574b833aeef29a24de5e2
Enceka <enceka@yeah.net>
2025-06-29 19:33:27 +0800

    Generate twrp device tree with twrpdtgen

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2025 The Android Open Source Project
+# Copyright (C) 2025 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_U30Air.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_U30Air-user \
+    omni_U30Air-userdebug \
+    omni_U30Air-eng
